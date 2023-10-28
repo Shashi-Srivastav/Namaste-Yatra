24847f33969782857aa3173d93177e0cab2e2bb7
Shashi Srivastava <Shashisrivastav96@gmail.com>
2023-10-28 10:18:49 +0530

    all listing routes

---
+++ routes/listing.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const router = express.Router();
+const wrapAsync = require("../utils/wrapAsync.js");
+const Listing = require("../models/listing.js");
+const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
+
+const listingController = require("../controllers/listings.js");
+const multer = require('multer');
+const {storage} = require("../cloudConfig.js");
+const upload = multer({storage});
+
+
+//using router.route to to dec lines of code
+router.route("/")
+    .get(wrapAsync(listingController.index))
+    .post(isLoggedIn,upload.single("listing[image]"), validateListing,wrapAsync(listingController.createListings));
+//New Route
+router.get("/new", isLoggedIn, listingController.renderNewForm);
+
+router.route("/:id")
+.get( wrapAsync(listingController.showListing))
+.put(isLoggedIn, isOwner,upload.single("listing[image]"), validateListing, wrapAsync(listingController.updateListing))
+.delete( isLoggedIn, isOwner, wrapAsync(listingController.destroyListing));
+
+
+//Edit Route
+router.get("/:id/edit", isLoggedIn, isOwner, wrapAsync(listingController.renderEditForm));
+
+module.exports = router;
