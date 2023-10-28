50636df33bda0b594ead037e73e780105f61a194
Shashi Srivastava <Shashisrivastav96@gmail.com>
2023-10-28 10:18:27 +0530

    index

---
+++ init/index.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const initData = require("./data.js");
+const Listing = require("../models/listing.js");
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+
+main()
+  .then(() => {
+    console.log("connected to DB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+async function main() {
+  await mongoose.connect(MONGO_URL);
+}
+
+const initDB = async () => {
+  await Listing.deleteMany({});
+  initData.data = initData.data.map((obj) => ({
+    ...obj,
+    owner: "653bcb5c83f3df2624f3cb77",
+  }));
+  await Listing.insertMany(initData.data);
+  console.log("data was initialized");
+};
+
+initDB();
+
+// user = Shashi
+// password = Shashi
