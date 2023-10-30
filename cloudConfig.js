6faab016c17c408c3e37110f3a6ab2087cdc54e3
Shashi Srivastava <Shashisrivastav96@gmail.com>
2023-10-30 07:17:22 +0530

    added limit to img upload

--- cloudConfig.js
+++ cloudConfig.js
@@ -1,6 +1,6 @@
 const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
-
+const multer = require('multer'); // Import multer
 
 cloudinary.config({
     cloud_name: process.env.CLOUD_NAME,
@@ -8,16 +8,33 @@ cloudinary.config({
     api_secret: process.env.CLOUD_API_SECRET
 });
 
-
+// Set a storage object with your Cloudinary settings
 const storage = new CloudinaryStorage({
     cloudinary: cloudinary,
     params: {
-      folder: 'wanderlust_DEV',
-      allowedFormats: ["png","jpg","jpeg"],
+        folder: 'wanderlust_DEV',
+        allowedFormats: ["png", "jpg", "jpeg"],
     },
-  });
+});
 
-  module.exports ={
+// Create a multer instance
+const upload = multer({
+    storage: storage,
+    // Add a file size limit check (2MB = 2 * 1024 * 1024 bytes)
+    limits: { fileSize: 2 * 1024 * 1024 },
+}).single('image'); // Use single file upload
+
+// Middleware for checking the file size before uploading to Cloudinary
+const fileSizeLimit = (req, res, next) => {
+  if (req.file && req.file.size > 2 * 1024 * 1024) {
+      req.flash("error", "File size too large. Max file size is 2MB.");
+      return res.redirect("/listings/new"); // Redirect to the new listing page
+  }
+  next();
+};
+
+module.exports = {
     cloudinary,
     storage,
-  }
+    fileSizeLimit, // Export the middleware for use in your routes/controllers
+};
