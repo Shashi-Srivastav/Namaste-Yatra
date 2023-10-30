f32bcae2d8f41855ff17599a72ee6451c5eb94c2
Shashi Srivastava <Shashisrivastav96@gmail.com>
2023-10-30 07:18:01 +0530

    added limit in schema

--- routes/listing.js
+++ routes/listing.js
@@ -3,27 +3,26 @@ const router = express.Router();
 const wrapAsync = require("../utils/wrapAsync.js");
 const Listing = require("../models/listing.js");
 const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
+const { fileSizeLimit } = require("../cloudConfig.js"); // Import the fileSizeLimit middleware
 
 const listingController = require("../controllers/listings.js");
 const multer = require('multer');
 const {storage} = require("../cloudConfig.js");
 const upload = multer({storage});
 
-
-//using router.route to to dec lines of code
+// Using router.route to reduce lines of code
 router.route("/")
     .get(wrapAsync(listingController.index))
-    .post(isLoggedIn,upload.single("listing[image]"), validateListing,wrapAsync(listingController.createListings));
-//New Route
+    .post(isLoggedIn, upload.single("listing[image]"), fileSizeLimit, validateListing, wrapAsync(listingController.createListings)); // Add fileSizeLimit middleware
+
+// New Route
 router.get("/new", isLoggedIn, listingController.renderNewForm);
 
 router.route("/:id")
-.get( wrapAsync(listingController.showListing))
-.put(isLoggedIn, isOwner,upload.single("listing[image]"), validateListing, wrapAsync(listingController.updateListing))
-.delete( isLoggedIn, isOwner, wrapAsync(listingController.destroyListing));
-
-
-//Edit Route
+    .get(wrapAsync(listingController.showListing))
+    .put(isLoggedIn, isOwner, upload.single("listing[image]"), fileSizeLimit, validateListing, wrapAsync(listingController.updateListing)) // Add fileSizeLimit middleware
+    .delete( isLoggedIn, isOwner, wrapAsync(listingController.destroyListing));
+// Edit Route
 router.get("/:id/edit", isLoggedIn, isOwner, wrapAsync(listingController.renderEditForm));
 
 module.exports = router;
