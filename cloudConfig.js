69b2ea12897da3e07be617befa848417b43816e2
Shashi Srivastava <Shashisrivastav96@gmail.com>
2023-10-30 22:07:45 +0530

    removed file size limit

--- cloudConfig.js
+++ cloudConfig.js
@@ -16,25 +16,7 @@ const storage = new CloudinaryStorage({
         allowedFormats: ["png", "jpg", "jpeg"],
     },
 });
-
-// Create a multer instance
-const upload = multer({
-    storage: storage,
-    // Add a file size limit check (2MB = 2 * 1024 * 1024 bytes)
-    limits: { fileSize: 2 * 1024 * 1024 },
-}).single('image'); // Use single file upload
-
-// Middleware for checking the file size before uploading to Cloudinary
-const fileSizeLimit = (req, res, next) => {
-  if (req.file && req.file.size > 2 * 1024 * 1024) {
-      req.flash("error", "File size too large. Max file size is 2MB.");
-      return res.redirect("/listings/new"); // Redirect to the new listing page
-  }
-  next();
-};
-
 module.exports = {
     cloudinary,
     storage,
-    fileSizeLimit, // Export the middleware for use in your routes/controllers
 };
